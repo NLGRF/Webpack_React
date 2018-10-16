18f435cfd416746eb86fa13266319957e9f8e81c
Non <non.devtech@gmail.com>
2018-10-16 16:53:35 +0700

    Webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,18 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: resolve(__dirname, 'dist'),
+  },module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env', 'babel-preset-react'],
+          },
+        },
+      },
+    ],
   },
 }
